86566e6495f46999c97cfe86560edcd117cda7a5
Raphael Nowominski <raph.nowo@gmail.com>
2018-05-28 01:19:05 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        'déclaration des variables 
+        Dim valeurentree As String = ""
+        Dim valeur1 As Double
+        Dim valeur2 As Double
+        'recupération du 1er nombre
+
+        Console.WriteLine(" Addition de deux nombres")
+            Do
+                Console.WriteLine("Entrer le premier nombre")
+
+            valeurentree = Console.ReadLine()
+        Loop Until IsNumeric(valeurentree)
+        valeur1 = valeurentree
+
+        ' récupération du 2ème nombre
+        Do
+            Console.WriteLine("Entrer la deuxième valeur")
+            valeurentree = Console.ReadLine()
+        Loop Until IsNumeric(valeurentree)
+        valeur2 = valeurentree
+
+        'addition 
+        Console.WriteLine(valeur1 & " + " & valeur2 & " = " & valeur1 + valeur2)
+
+        Console.Read()
+
+        MsgBox("test")
+    End Sub
+
+End Module
